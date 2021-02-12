b626bd30a8da542fb20b245bce6ddea7a4609695
Bruno Giacobo Pinto <bruno.giacobo@viaflow.com.br>
2021-02-12 07:39:20 -0300

    feat(app): add logger and base exports
    Add logger and base exports

--- base/lib/base.dart
+++ base/lib/base.dart
@@ -1,1 +1,10 @@
 library base;
+
+export 'package:logger/logger.dart';
+
+export 'src/extensions/boxconstraints_ext.dart';
+export 'src/navigation/action.dart';
+export 'src/navigation/middleware.dart';
+export 'src/state.dart';
+export 'src/view.dart';
+export 'src/viewmodell.dart';
